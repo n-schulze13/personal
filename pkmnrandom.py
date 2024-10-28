e7d09b55b9c7438c86478a4a5cfc20357cc134a4
Mr. Schulze <108143589+n-schulze13@users.noreply.github.com>
2024-10-28 13:18:19 +0000

    more update

--- pkmnrandom.py
+++ pkmnrandom.py
@@ -1,7 +1,18 @@
 # pokemon randomizer - will read the pokedex csv file and take specifications to output a list of mons for a draft!
 
-def main():
-    print("testing...")
+import pandas as pd
+import csv
+import sys
 
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python pkmnrandom.py pokemon.csv")
+        sys.exit(1)
+    
+    # read the csv into a variable
+    df = pd.read_csv(sys.argv[1])
+
+    print(df)
+    
 
 main()
